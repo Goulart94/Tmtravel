40f692fde4e500d3fb472fe816fc3b655f5aba63
Maycon Goulart <maycon_pbi@hotmail.com>
2022-03-10 14:18:10 +0000

    deploy netlify

--- .history/src/pages/_document_20220310134747.tsx
+++ .history/src/pages/_document_20220310134747.tsx
@@ -44,8 +44,6 @@ export default class MyDocument extends Document{
                <body>
                    <Main/>
                    <NextScript/>
-                
-                   
                </body>
            
            
